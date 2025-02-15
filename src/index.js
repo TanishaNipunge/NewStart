7da32ac0ac7fc2c673d53f9fdda698b39814596e
TanishaNipunge <tanishanipunge@gmail.com>
2025-02-15 23:12:48 +0530

    ApiError.js ,ApiResponce.js ,etc added

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ dotenv.config({
     path:'./env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port 8000:${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!",err)
+})
 
 /*import mongoose from 'mongoose';
 import {DB_NAME} from './constants';
